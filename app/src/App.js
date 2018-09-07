eb7aa2d86ce6d1b55634876e092c0368723a84ac
Peter Serwylo <peter@serwylo.com>
2018-09-07 23:08:35 +1000

    Stories + Facts laid out in boxes using React.

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = (props) =>
+    <div className="App">
+        <Story story={props.data.story} />
+    </div>;
+
+const Story = (props) =>
+    <div className="story">
+        Story
+        {props.story.facts.map(fact => <Fact fact={fact} />)}
+    </div>;
+
+const Fact = (props) =>
+    <div className="fact">
+        Fact {props.fact.label}
+    </div>;
 
 export default App;
--- app/src/index.js
+++ app/src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const data = {
+    story: {
+        label: "Kingston",
+        facts: [
+            {label: "It's great"},
+            {label: "It's okay..."},
+        ]
+    }
+};
+
+ReactDOM.render(<App data={data}/>, document.getElementById('root'));
 registerServiceWorker();

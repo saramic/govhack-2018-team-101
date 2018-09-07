4671f184e1f600fe34764bdf6feab7716768795c
Michael Milewski <saramic@gmail.com>
2018-09-08 05:37:16 +1000

    redux minimum setup

--- child-storyboard-app/src/index.js
+++ child-storyboard-app/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const reducer = (state = {}, action) => state;
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

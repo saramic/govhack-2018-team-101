31297fca619b12b7be6b8c5d41c69298955ddfb9
Peter Serwylo <peter@serwylo.com>
2018-09-08 13:36:04 +1000

    Add paper texture to background of comic, and add more test data.

--- app/src/rootReducer.js
+++ app/src/rootReducer.js
@@ -30,28 +30,84 @@ const defaultState = {
         image:
           "https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000071/132952l.jpg",
         dataSource: "http://data.gov.au",
-        panelType: 0
+        panelType: Math.round(Math.random() * 3),
       },
       {
         text: "Before Kingston was Kingston, it was known as Bootowa. Instead of English they spoke ...",
         image:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Aboriginal_Art_Australia%281%29.jpg/1280px-Aboriginal_Art_Australia%281%29.jpg",
         dataSource: "http://data.vic.gov.au",
-        panelType: 1
+        panelType: Math.round(Math.random() * 3),
       },
       {
         text: "It is a council (I think?)",
         image:
           "https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000057/118939l.jpg",
         dataSource: "http://kingston.gov.au",
-        panelType: 2
+        panelType: Math.round(Math.random() * 3),
       },
       {
         text: "There are people there",
         image:
           "https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000057/118933l.jpg",
         dataSource: "http://google.com",
-        panelType: 3
+        panelType: Math.round(Math.random() * 3),
+      },
+      {
+        text: "Kingston has heaps of grown ups to look after us.",
+        image:
+          "https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000071/132952l.jpg",
+        dataSource: "http://data.gov.au",
+        panelType: Math.round(Math.random() * 3),
+      },
+      {
+        text: "Before Kingston was Kingston, it was known as Bootowa. Instead of English they spoke ...",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Aboriginal_Art_Australia%281%29.jpg/1280px-Aboriginal_Art_Australia%281%29.jpg",
+        dataSource: "http://data.vic.gov.au",
+        panelType: Math.round(Math.random() * 3),
+      },
+      {
+        text: "It is a council (I think?)",
+        image:
+          "https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000057/118939l.jpg",
+        dataSource: "http://kingston.gov.au",
+        panelType: Math.round(Math.random() * 3),
+      },
+      {
+        text: "There are people there",
+        image:
+          "https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000057/118933l.jpg",
+        dataSource: "http://google.com",
+        panelType: Math.round(Math.random() * 3),
+      },
+      {
+        text: "Kingston has heaps of grown ups to look after us.",
+        image:
+          "https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000071/132952l.jpg",
+        dataSource: "http://data.gov.au",
+        panelType: Math.round(Math.random() * 3),
+      },
+      {
+        text: "Before Kingston was Kingston, it was known as Bootowa. Instead of English they spoke ...",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Aboriginal_Art_Australia%281%29.jpg/1280px-Aboriginal_Art_Australia%281%29.jpg",
+        dataSource: "http://data.vic.gov.au",
+        panelType: Math.round(Math.random() * 3),
+      },
+      {
+        text: "It is a council (I think?)",
+        image:
+          "https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000057/118939l.jpg",
+        dataSource: "http://kingston.gov.au",
+        panelType: Math.round(Math.random() * 3),
+      },
+      {
+        text: "There are people there",
+        image:
+          "https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000057/118933l.jpg",
+        dataSource: "http://google.com",
+        panelType: Math.round(Math.random() * 3),
       }
     ]
   }

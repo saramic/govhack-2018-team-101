d9693ad5666cb385829873fa4e5d6e754f05dc94
HENRY VO <HENRYVO94@GMAIL.com>
2018-09-08 18:20:41 +1000

    Adds rotating card

--- app/src/BuildStoryApp.js
+++ app/src/BuildStoryApp.js
@@ -1,49 +1,50 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-const BuildStoryApp = (props) =>
-    <div className="BuildStoryApp">
-        <h2>The Story of Matilda</h2>
-        <p>
-            A grade 6 school girl from Collingwood College using open data to explore her community
-            and build a story
-        </p>
-        <StoryBuilder
+const BuildStoryApp = props => (
+  <div className="BuildStoryApp">
+    <h2>The Story of Matilda</h2>
+    <p>
+      A grade 6 school girl from Collingwood College using open data to explore
+      her community and build a story
+    </p>
+    {/* <StoryBuilder
             newStories={props.newStories} />
         <StoryBoard
-            stories={props.stories} />
-    </div>;
+            stories={props.stories} /> */}
+  </div>
+);
 
-class StoryBuilder extends Component {
-    render() {
-        return (
-            <div className="StoryBuilder">
-                <h2>And then ...</h2>
-                <ul>
-                    {this.props.newStories.map((newStory) => {
-                            return (<li>{newStory}</li>)
-                        }
-                    )}
-                </ul>
-            </div>
-        )
-    }
-}
+// class StoryBuilder extends Component {
+//     render() {
+//         return (
+//             <div className="StoryBuilder">
+//                 <h2>And then ...</h2>
+//                 <ul>
+//                     {this.props.newStories.map((newStory) => {
+//                             return (<li>{newStory}</li>)
+//                         }
+//                     )}
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
 
-class StoryBoard extends Component {
-    render() {
-        return (
-            <div className="StoryBoard">
-                <h2>The story so far</h2>
-                <ul>
-                    {this.props.stories.map((story) => {
-                            return (<li>{story}</li>)
-                        }
-                    )}
-                </ul>
-            </div>
-        )
-    }
-}
+// class StoryBoard extends Component {
+//     render() {
+//         return (
+//             <div className="StoryBoard">
+//                 <h2>The story so far</h2>
+//                 <ul>
+//                     {this.props.stories.map((story) => {
+//                             return (<li>{story}</li>)
+//                         }
+//                     )}
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
 
 export default BuildStoryApp;
--- app/src/components/AddStoryPanel.js
+++ app/src/components/AddStoryPanel.js
@@ -1,45 +1,86 @@
-import {Component} from "react";
-import {Modal} from "antd";
+import { Component } from "react";
+import { Modal, Button } from "antd";
 import React from "react";
+import "../Story.css";
+
+const Card = ({ visible, data }) => {
+  return (
+    <div className={visible ? "card" : "card card--back"}>
+      <div class="card__side card__front">
+        <img src="https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000071/132952l.jpg" />
+      </div>
+      <div class="card__back">
+        <img src="https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000071/132952l.jpg" />
+
+        <Button onClick>Submit</Button>
+      </div>
+    </div>
+  );
+};
 
 export default class AddStoryPanel extends Component {
+  state = { visible: true, frontShown: true };
 
-    state = { visible: false };
+  showForm = () => {
+    this.setState({
+      visible: true
+    });
+  };
 
-    showForm = () => {
-        this.setState({
-            visible: true
-        });
-    };
+  hideForm = () => {
+    this.setState({
+      visible: false
+    });
+  };
 
-    hideForm = () => {
-        this.setState({
-            visible: false
-        });
-    };
+  showCardFront = () => {
+    this.setState({ frontShown: false });
+  };
 
-    render = () => {
-        return (
-            <div className="story-panel story-panel-new">
-                <a href="#" onClick={e => { e.preventDefault();  this.showForm(); }}>
-                    <div className="content">
-                        <div className="add icon">+</div>
-                        <div className="add text">Add</div>
-                    </div>
-                </a>
-                <Modal
-                    title={null}
-                    visible={this.state.visible}
-                    onOk={this.hideForm}
-                    onCancel={this.hideForm}
-                    footer={null}>
+  render = () => {
+    return (
+      <div className="story-panel story-panel-new">
+        <a
+          href="#"
+          onClick={e => {
+            e.preventDefault();
+            this.showForm();
+          }}
+        >
+          <div className="content">
+            <div className="add icon">+</div>
+            <div className="add text">Add</div>
+          </div>
+        </a>
+        <Modal
+          title={null}
+          visible={this.state.visible}
+          onOk={this.hideForm}
+          onCancel={this.hideForm}
+          footer={null}
+        >
+          <Card visible={this.state.frontShown} />
 
-                    {/*<FrontOfCard visible={this.state.showFront} />
-                    <BackOfCard />*/}
-
-                </Modal>
-
+          {this.state.frontShown ? (
+            <div>
+              <Button
+                className="card__button--right"
+                type="primary"
+                shape="circle"
+                icon="check"
+                onClick={this.showCardFront}
+              />
+              <Button
+                className="card__button--left"
+                type="primary"
+                shape="circle"
+                icon="close"
+                onClick={this.showNextStory}
+              />
             </div>
-        );
-    }
+          ) : null}
+        </Modal>
+      </div>
+    );
+  };
 }

eb3ffe14c5e3a706144273cecbf150a1ff563fa9
Michael Milewski <saramic@gmail.com>
2018-09-08 23:20:11 +1000

    home page MF

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Button } from 'antd';
+import './home.css'
 
 const Home = () => {
-  return <div>Hello, build your app</div>;
+
+
+  return <div className="background">
+    <div className="home-panel">
+      <h1>My Local Story</h1>
+      <p>Some shit about what this app does goes here...</p>
+      <Button type="primary">Create My Story</Button>
+    </div>
+  </div>
 };
 
 export default Home;

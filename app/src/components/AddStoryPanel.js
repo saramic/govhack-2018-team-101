47bc7c1d035feb57dcb54b9a2f34ad3b4d826e34
Peter Serwylo <peter@serwylo.com>
2018-09-08 21:34:44 +1000

    Show real data from stories in the tinder popup. Still requires formatting, but the content is now real data.

--- app/src/components/AddStoryPanel.js
+++ app/src/components/AddStoryPanel.js
@@ -4,18 +4,17 @@ import React from "react";
 import "../Story.css";
 import CardDataSource from "./CardDataSource";
 
-const Card = ({ visible, data, closeModal, onAcceptProposedStoryPanel }) => {
+const Card = ({ visible, data, closeModal, onAcceptProposedStoryPanel, proposedStoryAddition }) => {
   return (
     <div className={visible ? "card" : "card card--back"}>
       <div className="card__side card__front">
-        <h2>Do you want to see more penguins?</h2>
-        <img src="https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000071/132952l.jpg" />
+        <img src={proposedStoryAddition.image} />
       </div>
       <div className="card__back">
-        <img src="https://s3-ap-southeast-2.amazonaws.com/previews.dams.me/667/image/large/000/000/000/000/000000000000071/132952l.jpg" />
+        <img src={proposedStoryAddition.image} />
 
         <div className="card__data">
-          <p>Test</p>
+          <p>{proposedStoryAddition.template}</p>
 
           <CardDataSource />
 
@@ -43,6 +42,11 @@ export default class AddStoryPanel extends Component {
     this.props.onRejectProposedStoryPanel();
   };
 
+  acceptStory = () => {
+    this.setState({ frontShown: true });
+    this.props.onAcceptProposedStoryPanel();
+  };
+
   render = () => {
     return (
       <div className="story-panel story-panel-new">
@@ -58,34 +62,39 @@ export default class AddStoryPanel extends Component {
             <div className="add text">Add</div>
           </div>
         </a>
-        <Modal
-          title={null}
-          visible={this.props.proposedStoryAddition != null}
-          onOk={this.hideForm}
-          onCancel={this.hideForm}
-          footer={null}
-        >
-          <Card visible={this.state.frontShown} closeModal={this.hideForm} onAcceptProposedStoryPanel={this.props.onAcceptProposedStoryPanel} />
+        {this.props.proposedStoryAddition == null ? null :
+          <Modal
+            title={null}
+            visible={this.props.proposedStoryAddition != null}
+            onOk={this.hideForm}
+            onCancel={this.hideForm}
+            footer={null}
+          >
+            <Card
+              visible={this.state.frontShown}
+              closeModal={this.hideForm}
+              onAcceptProposedStoryPanel={this.acceptStory}
+              proposedStoryAddition={this.props.proposedStoryAddition}/>
 
-          {this.state.frontShown ? (
-            <div>
-              <Button
-                className="card__button--right"
-                type="primary"
-                shape="circle"
-                icon="check"
-                onClick={this.showCardFront}
-              />
-              <Button
-                className="card__button--left"
-                type="primary"
-                shape="circle"
-                icon="close"
-                onClick={this.showNextStory}
-              />
-            </div>
-          ) : null}
-        </Modal>
+            {this.state.frontShown ? (
+              <div>
+                <Button
+                  className="card__button--right"
+                  type="primary"
+                  shape="circle"
+                  icon="check"
+                  onClick={this.showCardFront}
+                />
+                <Button
+                  className="card__button--left"
+                  type="primary"
+                  shape="circle"
+                  icon="close"
+                  onClick={this.showNextStory}
+                />
+              </div>
+            ) : null}
+          </Modal>}
       </div>
     );
   };

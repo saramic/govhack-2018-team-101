f477e18dafe82ea03c9c22c5a54bcb8fac0aa5e9
selenasmall <selenawiththetattoo@gmail.com>
2018-09-09 12:43:00 +1000

    gimme credit

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Talk
 
-Introduction
+### Introduction
 
 Hi we are team 101 and have had a great time putting together our Story Board generator
 for this years Gov Hack 2018. We have taken a different angle on joining disperate data
@@ -17,14 +17,14 @@ and the next generation rather then telling them about it, show them and let the
 and show to others story books they create
 
 
-App run through
+### App run through
 
 So let's get started.
 
 Let's imagine we are 11 year old Matilda living in the seaside City of Kingston going
 to 5th grade in primary school. She can navigate to her already started story. A great
 place to start is at her school. She goes to Parkdale primary school. This can be seen
-in the first frame of the comic book like feel of story bory board generator
+in the first frame of the comic book like feel of story board generator
 
 At any time the info button can clicked to review the data source the we have been lucky
 to use. In the case of schools it is the data.vic School locations data. Let's click and
@@ -34,7 +34,7 @@ Parkdale Primary.
 So from school Matilda was prompted to play some ball sports. She thought this was a
 good idea and swiped right to accept and add this to her story.
 
-[read] "at the park she saw some rubbish and decided to put it in the bin" - this brings
+[child reading] "at the park I saw some rubbish and decided to put it in the bin" - this brings
 in data from ... and allows the child to link the data from the sports areas to the
 recycling data source - amazing.
 
--- app/src/pages/Credits.js
+++ app/src/pages/Credits.js
@@ -1,36 +1,131 @@
 import React from "react";
+import { Card, Icon, Row, Col } from 'antd';
+import "./Credits.css";
+
+const { Meta } = Card;
 
 const Credits = () => {
+
   return (
-    <div>
-      <h1>Credits</h1>
-      <p>Data sources:</p>
-      <ul>
-        <li>School Locations 'https://2018.hackerspace.govhack.org/data_sets/295' 'https://www.data.vic.gov.au/data/dataset/school-locations-2018'</li>
-        <li>Sport and Recreation Facilities 'https://2018.hackerspace.govhack.org/data_sets/275' 'https://www.data.vic.gov.au/data/dataset/sport-and-recreational-facilities'</li>
-        <li>LCA kerbside recycling calculator 'https://2018.hackerspace.govhack.org/data_sets/274' 'https://www.data.vic.gov.au/data/dataset/lca-kerbside-recycling-calculator'</li>
-        <li>Aged Care 'https://2018.hackerspace.govhack.org/data_sets/346' 'https://www.aihw.gov.au/reports/older-people/older-australia-at-a-glance/contents/summary'</li>
-        <li>Crime Stats Agency 'https://2018.hackerspace.govhack.org/data_sets/300' 'https://www.crimestatistics.vic.gov.au/crime-statistics/latest-crime-data'</li>
-        <li>Melbourne Urban Forest Visual 'https://data.melbourne.vic.gov.au/'</li>
-        <li>2016 Community Profiles 'https://2018.hackerspace.govhack.org/data_sets/269' 'https://www.multicultural.vic.gov.au/population-and-migration/victorias-diversity/community-profiles-2016-census'</li>
-        <li>2018 Employment Projections 'https://2018.hackerspace.govhack.org/data_sets/116' 'http://lmip.gov.au/default.aspx?LMIP/GainInsights/EmploymentProjections'</li>
-        <li></li>
-      </ul>
+    <div className={"credits"}>
+      <h1>Data Sources</h1>
+        <div style={{ padding: '0px 150px' }}>
+            <Row gutter={16}>
+                <Col span={8}>
+                    <Card
+                        style={{ margin: '10px auto' }}
+                        cover={<img alt="example" src="https://static.thenounproject.com/png/653991-200.png" />}
+                        actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}>
+                    <Meta
+                        title="School Locations"
+                        description={<a href="https://2018.hackerspace.govhack.org/data_sets/295">https://2018.hackerspace.govhack.org/data_sets/295</a>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        style={{ margin: '10px auto' }}
+                        cover={<img alt="example" src="http://icons.iconarchive.com/icons/icons8/android/256/Sports-Football-icon.png" />}
+                        actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}>
+                    <Meta
+                        title="Sport and Recreation Facilities"
+                        description={<a href="https://2018.hackerspace.govhack.org/data_sets/275">https://2018.hackerspace.govhack.org/data_sets/275</a>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        style={{ margin: '10px auto' }}
+                        cover={<img alt="example" src="https://png.icons8.com/recycling/win8/1600" />}
+                        actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}>
+                    <Meta
+                        title="LCA kerbside recycling calculator"
+                        description={<a href="https://2018.hackerspace.govhack.org/data_sets/274">https://2018.hackerspace.govhack.org/data_sets/274</a>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        style={{ margin: '10px auto' }}
+                        cover={<img alt="example" src="https://static.thenounproject.com/png/172756-200.png" />}
+                        actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}>
+                    <Meta
+                        title="Aged Care"
+                        description={<a href="https://2018.hackerspace.govhack.org/data_sets/346">https://2018.hackerspace.govhack.org/data_sets/346</a>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        style={{ margin: '10px auto' }}
+                        cover={<img alt="example" src="https://static.thenounproject.com/png/8847-200.png" />}
+                        actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}>
+                    <Meta
+                        title="Crime Stats Agency"
+                        description={<a href="https://2018.hackerspace.govhack.org/data_sets/300">https://2018.hackerspace.govhack.org/data_sets/300</a>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        style={{ margin: '10px auto' }}
+                        cover={<img alt="example" src="https://static.thenounproject.com/png/31668-200.png" />}
+                        actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}>
+                    <Meta
+                        title="Melbourne Urban Forest Visual"
+                        description={<a href="https://data.melbourne.vic.gov.au/">https://data.melbourne.vic.gov.au/</a>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        style={{ margin: '10px auto' }}
+                        cover={<img alt="example" src="http://cdn.onlinewebfonts.com/svg/img_460058.png" />}
+                        actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}>
+                    <Meta
+                        title="2016 Community Profiles"
+                        description={<a href="https://2018.hackerspace.govhack.org/data_sets/269">https://2018.hackerspace.govhack.org/data_sets/269</a>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        style={{ margin: '10px auto' }}
+                        cover={<img alt="example" src="https://static.thenounproject.com/png/42421-200.png" />}
+                        actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}>
+                    <Meta
+                        title="2018 Employment Projections"
+                        description={<a href="https://2018.hackerspace.govhack.org/data_sets/116">https://2018.hackerspace.govhack.org/data_sets/116</a>}
+                        />
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card
+                        style={{ margin: '10px auto' }}
+                        cover={<img alt="example" src="http://cdn.onlinewebfonts.com/svg/img_574465.png" />}
+                        actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}>
+                    <Meta
+                        title="Indigenous Health and Welfare services"
+                        description={<a href="https://2018.hackerspace.govhack.org/data_sets/354">https://2018.hackerspace.govhack.org/data_sets/354</a>}
+                        />
+                    </Card>
+                </Col>
+            </Row>
+            </div>
     </div>
   );
 };
 
 export default Credits;
 
-// {
-//     id: "indigenous",
-//     template: "[NAME] indigenous",
-//     image: "https://cdn.newsapi.com.au/image/v1/6de41d261846a75fee88dcf92a67efc9",
-//     dataSource: {
-//         name: '',
-//         hackerspace_id: 'https://2018.hackerspace.govhack.org/data_sets/ ???',
-//         link: '',
-//         visualisation: 'N/A',
-//     },
-//     nextElements: [],
-// }
+// <ul>
+//   <li>School Locations 'https://2018.hackerspace.govhack.org/data_sets/295' 'https://www.data.vic.gov.au/data/dataset/school-locations-2018'</li>
+//   <li>Sport and Recreation Facilities 'https://2018.hackerspace.govhack.org/data_sets/275' 'https://www.data.vic.gov.au/data/dataset/sport-and-recreational-facilities'</li>
+//   <li>LCA kerbside recycling calculator 'https://2018.hackerspace.govhack.org/data_sets/274' 'https://www.data.vic.gov.au/data/dataset/lca-kerbside-recycling-calculator'</li>
+//   <li>Aged Care 'https://2018.hackerspace.govhack.org/data_sets/346' 'https://www.aihw.gov.au/reports/older-people/older-australia-at-a-glance/contents/summary'</li>
+//   <li>Crime Stats Agency 'https://2018.hackerspace.govhack.org/data_sets/200' 'https://www.crimestatistics.vic.gov.au/crime-statistics/latest-crime-data'</li>
+//   <li>Melbourne Urban Forest Visual 'https://data.melbourne.vic.gov.au/'</li>
+//   <li>2016 Community Profiles 'https://2018.hackerspace.govhack.org/data_sets/269' 'https://www.multicultural.vic.gov.au/population-and-migration/victorias-diversity/community-profiles-2016-census'</li>
+//   <li>2018 Employment Projections 'https://2018.hackerspace.govhack.org/data_sets/116' 'http://lmip.gov.au/default.aspx?LMIP/GainInsights/EmploymentProjections'</li>
+//   <li>Indigenous Health and Welfare services 'https://2018.hackerspace.govhack.org/data_sets/354' 'https://www.aihw.gov.au/reports-statistics/health-welfare-services/indigenous-health-welfare-services/data'</li>
+// </ul>

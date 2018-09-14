630ab19120b400f55f1aa8c8a53145004be8f0a0
Michael Milewski <saramic@gmail.com>
2018-09-14 23:59:22 +1000

    simple refactor to destructure data object

--- app/src/components/DataVisualization/DataVisualization.js
+++ app/src/components/DataVisualization/DataVisualization.js
@@ -1,19 +1,19 @@
 import React from "react";
 
-const DataVisualization = ({ data }) => {
+const DataVisualization = ({data: {visualisation, name, link, hackerspace_id}}) => {
   return (
     <div className="visualization">
-      {data.visualisation ? (
-        <img src={"/images/viz/" + data.visualisation} />
+      {visualisation ? (
+        <img src={"/images/viz/" + visualisation}/>
       ) : null}
-      <h1>{data.name}</h1>
+      <h1>{name}</h1>
       <p>
-        <a href={data.link} target="_blank">
+        <a href={link} target="_blank">
           Original Data Source
         </a>
       </p>
       <p>
-        <a href={data.hackerspace_id} target="_blank">
+        <a href={hackerspace_id} target="_blank">
           GovHack 2018 Hackerspace Info
         </a>
       </p>
